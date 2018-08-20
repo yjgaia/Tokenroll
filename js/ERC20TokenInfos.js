7f3064010b0cb040fa687a3da72579ac6c56a13f
YJ <hanul@hanul.me>
2018-08-21 03:51:48 +0900

    no message

--- js/ERC20TokenInfos.js
+++ js/ERC20TokenInfos.js
@@ -1,5 +1,5 @@
 Tokenroll.ERC20TokenInfos = {
-	"0xe41d2489571d322189246dafa5ebde1f4699f498" : {
+	'0xe41d2489571d322189246dafa5ebde1f4699f498' : {
 		name : 'ZRX',
 		icon : 'tokenicon/0x.png',
 		site : 'https://0xproject.com/'
